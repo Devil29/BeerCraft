5523398a1521452760111837b8d7e23909ddfb0c
Devil29 <vishalraj48@gmail.com>
2019-04-22 22:09:38 +0530

    using Axios to make an api call to firebase and fetch complete beer data

--- src/components/SearchResultPage.js
+++ src/components/SearchResultPage.js
@@ -1,60 +1,31 @@
 import React, {Component} from 'react'
-
+import axios from 'axios'
 
 // http://starlord.hackerearth.com/beercraft
 // To get this value from store which which get from api
-function temporayBeerData() {
-    let beerData = [
-        {
-            "abv":"0.05",
-            "ibu":"",
-            "id":1436,
-            "name":"Pub Beer",
-            "style":"American Pale Lager",
-            "ounces":12.0
-         },
-         {
-            "abv":"0.06",
-            "ibu":"",
-            "id":2265,
-            "name":"Devils Cup",
-            "style":"American Pale Ale (APA)",
-            "ounces":12.0
-         },
-         {
-            "abv":"0.07",
-            "ibu":"",
-            "id":2264,
-            "name":"Rise of the Phoenix",
-            "style":"American IPA",
-            "ounces":12.0
-         },
-         {
-            "abv":"0.09",
-            "ibu":"",
-            "id":2263,
-            "name":"Sinister",
-            "style":"American Double / Imperial IPA",
-            "ounces":12.0
-         },
-         {
-            "abv":"0.07",
-            "ibu":"",
-            "id":2262,
-            "name":"Sex and Candy",
-            "style":"American IPA",
-            "ounces":12.0
-         }
-    ]
-    return beerData;
-}
 
 class SearchResultPage extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            beerData: []
+        }
+    }
+    temporayBeerData() {
+        axios.get('https://vue-js-e14f0.firebaseio.com/beers.json')
+        .then(res => {
+            console.log(res)
+            this.setState({beerData: res.data});
+        })
+        .catch(error => console.log(error))
+    }
     render(){
+        this.temporayBeerData();
         return (
             <div>
                 <ul>
-                {temporayBeerData().map((value, index) => {
+                {this.state.beerData.map((value, index) => {
                     return <li key={index}>{value.name}</li>
                 })}
                 </ul>
