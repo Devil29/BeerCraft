6450d974078686dd5816f2810f406590faadc8ff
anubhav253 <anubhav253kumar@gmail.com>
2019-04-23 17:54:05 +0530

    Design refactored for address page

--- src/components/AddressPage.js
+++ src/components/AddressPage.js
@@ -10,13 +10,22 @@ class AddressPage extends Component {
     }
     render() {
         return (
-            <div className="row">
+            <div>
                 <button><Link to="/add-address">Add Address</Link></button>
-                <ul>
+                <div className="row">
                 {addressData().map((value, index) => {
-                    return <li key={index}>{value.addressLine1} {value.addressLine2} {value.city}</li>
+                    return <div className="col-md-3">
+                        <div className="card-body">
+                            <p className="card-text">Flat number - {value.addressLine1}</p>
+                            <p className="card-text">Street  - {value.addressLine2}</p>
+                            <p className="card-text">City - {value.city}</p>
+                            <p className="card-text">State - {value.state}</p>
+                            <p className="card-text">Pincode - {value.pincode}</p>
+                            <p className="card-text">Mobile number - {value.mobileNumber}</p>
+                        </div>
+                    </div>
                 })}
-                </ul>
+                </div>
             </div>
         );
     }
